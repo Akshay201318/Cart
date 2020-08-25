8e22cbf65e6913ae86c4192d198ae121773dc727
Akshay <akshaypanchal201318@gmail.com>
2020-08-25 14:19:12 +0530

    Added action icons i.e Increase, Decrease and Delete

--- src/CartItem.js
+++ src/CartItem.js
@@ -2,9 +2,21 @@ import React from 'react';
 
 class CartItem extends React.Component{
 
+    constructor(){
+        super();
+        this.state={
+            price: 999,
+            title: 'Phone',
+            Qty : 1,
+            img : ''
+        }
+    }
+
     render(){
+        const {price, title, Qty, img}=this.state;
 
         return(
+              
 
             <div className="cart-item">
 
@@ -14,10 +26,14 @@ class CartItem extends React.Component{
 
                 </div>
                 <div className="right-block">
-                    <div>Phone</div>
-                    <div>Rs 99</div>
-                    <div>Qyt: 1</div>
+                    <div>{title}</div>
+                    <div>Rs {price}</div>
+                    <div>Qyt: {Qty}</div>
                     <div className="cart-item-actions">
+
+                        <img alt="Increase" className="action-icons" src="https://image.flaticon.com/icons/svg/992/992651.svg"/>
+                        <img alt="decrease" className="action-icons" src="https://image.flaticon.com/icons/svg/659/659892.svg"/>
+                        <img alt="Delete" className="action-icons" src="https://image.flaticon.com/icons/svg/3096/3096673.svg"/>
 
                     </div>
 
